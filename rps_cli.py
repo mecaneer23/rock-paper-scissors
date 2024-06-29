9e94723ea7f4b20aecd88b71c9ab1aa22933176a
Mecaneer23 <74385377+Mecaneer23@users.noreply.github.com>
2024-06-29 13:40:30 -0500

    refactor: improve readability and fundamentally rewrite

--- rps_cli.py
+++ rps_cli.py
@@ -1,62 +1,88 @@
 #!/usr/bin/env python3
+"""
+Logic for a Rock Paper Scissors game.
+When run directly, play using a command line interface.
+"""
 
+from enum import Enum
 from random import choice
+from typing import Type, TypeVar
 
 
-def letter_to_name(letter):
-    names = {"r": "Rock", "p": "Paper", "s": "Scissors", "q": "Quit"}
-    if letter in names:
-        return names[letter]
-    else:
-        print("Invalid input!")
-        main()
+class RPS(Enum):
+    """Represent a rock, paper, or scissors object"""
+
+    ROCK = "Rock"
+    PAPER = "Paper"
+    SCISSORS = "Scissors"
+
+    def __gt__(self, other: "RPS") -> bool:
+        if self == other:
+            return False
+        return (
+            (self == RPS.PAPER and other == RPS.ROCK)
+            or (self == RPS.ROCK and other == RPS.SCISSORS)
+            or (self == RPS.SCISSORS and other == RPS.PAPER)
+        )
+
+    def __str__(self) -> str:
+        return self.value
 
 
-def main(
-    user_move=None,
-    computer_move=choice(["r", "p", "s"]),
-):
-    if user_move is None:
-        user_move = input("Rock, Paper, or Scissors? (r, p, s), q to quit: ")
-    if user_move == "q":
-        return {
-            "user_move": user_move,
-            "computer_move": computer_move,
-            "result": "Goodbye!",
-        }
+_GenericEnum = TypeVar("_GenericEnum", bound=Enum)
+
+
+def _get_first_char_dict(enum: Type[_GenericEnum]) -> dict[str, _GenericEnum]:
+    """
+    Return a dictionary mapping the first letter of each Enum
+    item to the corresponding Enum item.
+    """
+    keys = [item.name for item in enum]
+    return dict(zip((key[0] for key in keys), enum))
+
+
+_FIRST_CHAR_DICT = _get_first_char_dict(RPS)
+
+
+def _str_to_rps(string: str) -> RPS:
+    if len(string) == 1:
+        return _FIRST_CHAR_DICT[string.upper()]
+    return RPS[string.upper()]
+
+
+class Result:
+    """Store a result from the rock paper scissors game"""
+
+    def __init__(self, user_move: RPS, computer_move: RPS):
+        self._user_move = user_move
+        self._computer_move = computer_move
+        self._result = ""
+
+    def set_result(self, result: str) -> "Result":
+        """Set a string containing how the game went"""
+        self._result = result
+        return self
+
+    def __str__(self) -> str:
+        return f"User's move: {self._user_move}\
+        \nComputer's move: {self._computer_move}\
+        \n{self._result}"
+
+
+def run_as_cli() -> Result:
+    """Run one turn of Rock, Paper, Scissors in the terminal"""
+    user_move = input("Rock, Paper, or Scissors? (r, p, s): ")
+    computer_move = choice(["r", "p", "s"])
+    output = Result(_str_to_rps(user_move), _str_to_rps(computer_move))
 
     if user_move == computer_move:
-        return {
-            "user_move": user_move,
-            "computer_move": computer_move,
-            "result": "Tie!",
-        }
-    elif (
-        (user_move == "r" and computer_move == "s")
-        or (user_move == "p" and computer_move == "r")
-        or (user_move == "s" and computer_move == "p")
-    ):
-        return {
-            "user_move": user_move,
-            "computer_move": computer_move,
-            "result": "You win!",
-        }
-    elif (
-        (user_move == "r" and computer_move == "p")
-        or (user_move == "p" and computer_move == "s")
-        or (user_move == "s" and computer_move == "r")
-    ):
-        return {
-            "user_move": user_move,
-            "computer_move": computer_move,
-            "result": "You lose!",
-        }
+        return output.set_result("Tie!")
+    if user_move > computer_move:
+        return output.set_result("You win!")
+    if user_move < computer_move:
+        return output.set_result("You lose!")
+    raise RuntimeError("Something went wrong!")
 
 
 if __name__ == "__main__":
-    result = main()
-    print(
-        f"User's move: {letter_to_name(result['user_move'])}\
-        \nComputer's move: {letter_to_name(result['computer_move'])}\
-        \n{result['result']}"
-    )
+    print(run_as_cli())
